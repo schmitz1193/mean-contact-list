76de56649bcaa176f0a629a4f3d8f8ceca5da6df
schmitz1193 <schmitz1193@gmail.com>
2016-03-11 17:32:22 -0600

    data from ctrl and server

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -4,4 +4,12 @@ const myApp = angular.module('myApp',[]);
 
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
   console.log("help from the controller");
+
+  //create route to  get our data from
+  $http.get('/contactlist').success(function(response){
+    console.log("I got the data I requested");
+    //puts the data into our HTML or our browser
+    $scope.contactlist = response;
+  })
+
 }]);
--- server.js
+++ server.js
@@ -10,5 +10,32 @@ const app = express();
 // tells server where to look to find them
 app.use(express.static(__dirname + '/public'));
 
+// tells the server to listen for the get request and log the message in the command prompt
+app.get('/contactlist', function(req,res){
+  console.log("I received a GET request")
+
+   const person1 = {
+      name: 'Tim',
+      email: 'tim@email.com',
+      number: '(111) 111-111'
+    };
+  const person2 = {
+      name: 'Emily',
+      email: 'emily@email.com',
+      number: '(222) 222-2222'
+    };
+  const person3 = {
+      name: 'John',
+      email: 'john@email.com',
+      number: '(333) 333-3333'
+    };
+
+    const contactlist = [person1, person2, person3];
+
+    //json is the format the data is in and responds to the res.send we are  requesting the data back in json format
+    res.json(contactlist);
+
+});
+
 app.listen(3000);
 console.log('Server running on port 3000');
