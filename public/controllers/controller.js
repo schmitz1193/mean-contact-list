4473718fcb817c1d65a612acc92210a9bc833a94
schmitz1193 <schmitz1193@gmail.com>
2016-03-12 16:20:56 -0600

    delete, clear, update

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -7,7 +7,7 @@ myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
 
 //put the get  in a function to allow the screen to refreshes when a new contact is added
 const refresh = function() {
-  //create route to  get our data from
+  //create route to GET our data from the server
   $http.get('/contactlist').success(function(response){
     console.log("I got the data I requested");
     //puts the data into our HTML or our browser
@@ -18,13 +18,46 @@ const refresh = function() {
 
 refresh();
 
+// POST request
 $scope.addContact = function() {
   console.log($scope.contact);
+  //send the new contact data to the server
   $http.post('/contactlist', $scope.contact).success(function(response){
     console.log("Response back ", response);
     refresh();  //call to reset data on page
   });
 };
 
+
+// DELETE request send id from client to the server
+$scope.remove = function(id) {
+  console.log(id);
+  $http.delete('/contactlist/' + id).success(function(response) {
+    refresh();
+  });
+};
+
+
+//GET request based on the id from the client, get that contact from the db
+$scope.edit = function(id) {
+  console.log(id);
+  $http.get('/contactlist/' + id).success(function(response) {
+    $scope.contact = response;
+  });
+};
+
+
+//PUT request based on the id on the scope, put those changes in the appropriate contact in the db
+$scope.update = function() {
+  console.log($scope.contact._id);
+  $http.put('/contactlist/' + $scope.contact._id, $scope.contact).success(function(response) {
+    refresh();
+  })
+};
+
+//just blank out the info in the input--changed my mind!
+$scope.deselect = function() {
+  $scope.contact = "";
+}
 
 }]);
--- server.js
+++ server.js
@@ -15,7 +15,17 @@ const bodyParser = require('body-parser');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
-// tells the server to listen for the get request and log the message in the command prompt
+//test the server
+//==========================================
+        //check that server is running
+          //when index file is requested, send hello world response to page
+        // app.get('/', function( req, res){
+        //  res.send("hello world, from server.js");
+        // });
+//==========================================
+
+
+// tells the server which template to use as the index & to listen for the get request and log the message in the command prompt
 app.get('/contactlist', function(req,res){
   console.log("I received a GET request")
   db.contactlist.find(function(err,docs) {
@@ -25,20 +35,24 @@ app.get('/contactlist', function(req,res){
     //json is the format the data is in and responds to the res.send we are  requesting the data back in json format
     res.json(docs);
   });
+});
 
-  app.post('/contactlist', function(req,res){
-    console.log("post request", req.body);
+//POST - place the new contact in the db
+app.post('/contactlist', function(req,res){
+  console.log("post request", req.body);
 
-    //insert into mongodb
-    //doc is the item we are posting
-    db.contactlist.insert(req.body, function(err,doc){
+  //insert into mongodb
+  //doc is the item we are posting
+  db.contactlist.insert(req.body, function(err,doc){
 
-      //in addition to posting, send data back to ctrlr on callback
-      res.json(doc);
-    });
-
+    //in addition to posting, send data back to ctrlr on callback
+    res.json(doc);
   });
 
+});
+
+
+  // use to test before db was created
   //  const person1 = {
   //     name: 'Tim',
   //     email: 'tim@email.com',
@@ -58,7 +72,44 @@ app.get('/contactlist', function(req,res){
   //   const contactlist = [person1, person2, person3];
 
 
+//DELETE request
+app.delete('/contactlist/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(id);
+  // "id" -which contact has been selected to be deleted by getting from req.params.id.  Then the db remove finds the record with the corresponding _id
+  db.contactlist.remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
+    res.json(doc);
+  });
 });
 
+
+//GET select something we want to change and get that  record to put in input fields
+app.get('/contactlist/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(id);
+  //using req.params.id from client and matching to _id to find right contact to modify
+  db.contactlist.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
+    res.json(doc);
+  });
+});
+
+//PUT the change to the contact into the correct place on the db
+app.put('/contactlist/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(req.body.name);
+  //using the req.params.id from client match _id in mongo to record you want to update. Pu the changed datat from req.body into the db vaiable names
+  db.contactlist.findAndModify({
+    query: {_id: mongojs.ObjectId(id)},
+    update: {$set: {
+      name: req.body.name,
+      email: req.body.email,
+      number: req.body.number}},
+    new: true}, function (err, doc) {
+      res.json(doc);
+    }
+  );
+});
+
+
 app.listen(3000);
-console.log('Server running on port 3000');
+console.log('At your service on port 3000');
